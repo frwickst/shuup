f591bdf8a0dadf5c86c3c80a5bff8265e2f52130
Mathieu Comandon <strycore@gmail.com>
2017-02-07 10:07:37 -0800

    Remove permission autodiscover from root urlconf

--- shuup_workbench/urls.py
+++ shuup_workbench/urls.py
@@ -9,10 +9,6 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-import permission
-
-permission.autodiscover()
-
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
